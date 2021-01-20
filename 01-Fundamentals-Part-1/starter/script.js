42218955cc3c890fbc9aa99bcac9a1395c05b99a
Carlos Mourullo <carlos@frontastic.cloud>
2021-01-20 09:29:10 +0000

    Section 2/10

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,10 @@
 let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!');
-
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+let firstName = "Jonas";
+
+let myFirstJob = 'Coder';
+
+console.log(myFirstJob);
