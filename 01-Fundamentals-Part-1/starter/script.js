fa68bb9833f92adab9da88749c66b2f7ee4fae57
Carlos Mourullo <carlos@frontastic.cloud>
2021-01-20 09:44:50 +0000

    Section 2/12

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,10 +1,34 @@
+/*
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
 console.log("Jonas");
 console.log(23);
-let firstName = "Jonas";
 
+let firstName = "Jonas";
+let PI = 3.1416;
 let myFirstJob = 'Coder';
 
 console.log(myFirstJob);
+*/
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof "text");
+
+javaScriptIsFun = "YES";
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof text);
+
+year = 1985;
+console.log(year);
+console.log(typeof text);
+
+console.log(typeof null);
