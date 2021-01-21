aa97fee642117f2c86e1b052f4d0b2bcd8440683
Carlos Mourullo <carlos@frontastic.cloud>
2021-01-21 09:26:48 +0000

    Assignment - Data Types

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,7 +1,14 @@
-let country = 'Ireland';
-let continent = 'Europe';
+let country = "Ireland";
+let continent = "Europe";
 let population = 4.9;
 
 console.log(country);
 console.log(continent);
 console.log(population);
+
+let isIsland = true;
+let language;
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
