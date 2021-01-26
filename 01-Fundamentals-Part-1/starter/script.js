e9b1ec0836822ddaa41aa900158cd1277e8ddb01
Carlos Mourullo <carlos@frontastic.cloud>
2021-01-26 10:01:56 +0000

    Section 2/15

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -48,6 +48,7 @@ lastName = "Flores";
 console.log(lastName);
 */
 
+/*
 // Math operators
 let thisYear = 2021;
 const ageJonas = thisYear - 1989;
@@ -75,3 +76,16 @@ console.log(ageSarah >= 18);
 const isFullAge = ageSarah >= 18;
 
 console.log(thisYear - 1989 > thisYear - 1990);
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
