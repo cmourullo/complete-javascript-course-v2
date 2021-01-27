fccd1140f07218c437e58baf25dbe3a63fcc35f3
Carlos Mourullo <carlos@frontastic.cloud>
2021-01-27 09:57:01 +0000

    Challenge 01

---
+++ 01-Fundamentals-Part-1/starter/challenge01.js
@@ -0,0 +1,17 @@
+// Test data 1
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+// Test data 2
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn = 1.76;
+
+const bmiMark = massMark / heightMark ** 2;
+const bmiJohn = massJohn / heightJohn ** 2;
+const markHeigherBMI = bmiMark > bmiJohn;
+
+console.log(bmiMark, bmiJohn, markHeigherBMI);
