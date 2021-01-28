83937f66b2ad03789b6ef307389e9dd1f4604a58
Carlos Mourullo <carlos@frontastic.cloud>
2021-01-28 09:38:30 +0000

    Assignment - Strings and Template Literals + Taking Decisions

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -23,13 +23,25 @@ console.log(population > findlandPopulation);
 let averagePopulation = 33;
 console.log(population < averagePopulation);
 
-let description =
-  country +
-  " is in " +
-  continent +
-  ", and its " +
-  population +
-  " million people speak " +
-  language;
+// let description =
+//   country +
+//   " is in " +
+//   continent +
+//   ", and its " +
+//   population +
+//   " million people speak " +
+//   language;
+
+let description = `${country} is in ${continent}, and its ${population} million people speal ${language}`;
 
 console.log(description);
+
+let message;
+if (population > averagePopulation) {
+  message = `${country}'s population is above average`;
+} else {
+  let millionsBelow = averagePopulation - population;
+  message = `${country}'s population is ${millionsBelow} millions below average`;
+}
+
+console.log(message);
