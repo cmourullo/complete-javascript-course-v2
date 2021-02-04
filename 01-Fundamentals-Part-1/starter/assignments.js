55dfe10e368465006548a259edef986b5d7c748d
Carlos Mourullo <carlos@frontastic.cloud>
2021-02-04 09:42:13 +0000

    Assignment - Logical Operators

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -44,22 +44,28 @@ if (population > averagePopulation) {
   message = `${country}'s population is ${millionsBelow} millions below average`;
 }
 
-console.log(message);
+// console.log(message);
 
-console.log("9" - "5"); // 4
-console.log("19" - "13" + "17"); // 617
-console.log("19" - "13" + 17); // 23
-console.log("123" < 57); // false
-console.log(5 + 6 + "4" + 9 - 4 - 2); // 1143
+// console.log("9" - "5"); // 4
+// console.log("19" - "13" + "17"); // 617
+// console.log("19" - "13" + 17); // 23
+// console.log("123" < 57); // false
+// console.log(5 + 6 + "4" + 9 - 4 - 2); // 1143
 
-const numNeighbours = Number(
-  prompt("How many neighbour countries does your country have?")
-);
+// const numNeighbours = Number(
+//   prompt("How many neighbour countries does your country have?")
+// );
 
-if (numNeighbours === 1) {
-  console.log(`Only ${numNeighbours} border`);
-} else if (numNeighbours > 1) {
-  console.log("More than 1 border");
+// if (numNeighbours === 1) {
+//   console.log(`Only ${numNeighbours} border`);
+// } else if (numNeighbours > 1) {
+//   console.log("More than 1 border");
+// } else {
+//   console.log("No borders");
+// }
+
+if (language === "english" && population < 50 && !isIsland) {
+  console.log(`You should live in ${country} :)'`);
 } else {
-  console.log("No borders");
+  console.log(`${country} does not meet your criteria :('`);
 }
