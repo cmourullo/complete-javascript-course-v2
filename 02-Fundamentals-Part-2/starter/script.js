fe3a9d875754f964cf113a66bf089a391f51e9c5
Carlos Mourullo <carlos@frontastic.cloud>
2021-05-19 09:40:02 +0200

    Section 3/44

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -184,6 +184,7 @@ const jonas = {
 console.log(jonas);
 */
 
+/*
 const jonas = {
   firstName: "Jonas",
   lastName: "Schmedtmann",
@@ -221,3 +222,43 @@ console.log(jonas);
 console.log(
   `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`
 );
+*/
+
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  birthYear: 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+  hasDriversLicense: true,
+
+  // function value
+  // calcAge: function (birthYear) {
+  //   return 2037 - birthYear;
+  // },
+  // calcAge: function () {
+  // console.log(this);
+  // return 2037 - this.birthYear;
+  // },
+
+  calcAge: function () {
+    this.age = 2037 - this.birthYear;
+    return this.age;
+  },
+
+  getSummary: function () {
+    return `${this.firstName} is a ${this.calcAge()}-years old ${
+      this.job
+    } and he has ${this.hasDriversLicense ? "a" : "no"} driver's licence`;
+  },
+};
+
+// console.log(jonas.calcAge(1991));
+// console.log(jonas["calcAge"](1991));
+// console.log(jonas.calcAge(jonas.birthYear));
+// console.log(jonas.calcAge());
+// console.log(jonas.age);
+
+// Challenge
+// "Jonas is a 46-years old teacher and he has a driver's licence"
+console.log(jonas.getSummary());
