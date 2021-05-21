c386bffeab7c84b484ad5a3020b67cd0b55000ae
Carlos Mourullo <carlos@frontastic.cloud>
2021-05-21 09:46:47 +0200

    Section 3/47

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -265,6 +265,60 @@ const jonas = {
 console.log(jonas.getSummary());
 */
 
+/*
 for (let rep = 1; rep <= 10; rep++) {
   console.log(`Lifting weights repetitions ${rep}`);
 }
+*/
+
+const jonas = [
+  "Jonas",
+  "Schmedtmann",
+  2037 - 1991,
+  "teacher",
+  ["Michael", "Peter", "Steven"],
+];
+
+const types = [];
+
+for (let i = 0; i < jonas.length; i++) {
+  // Reading from jonas array
+  console.log(jonas[i], typeof jonas[i]);
+
+  // Filling types array
+  // types[i] = typeof jonas[i];
+  types.push(typeof jonas[i]);
+}
+
+console.log(types);
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+  ages.push(2037 - years[i]);
+}
+
+console.log(ages);
+
+console.log("--- ONLY WITH STRING ---");
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] !== "string") {
+    continue;
+  }
+
+  console.log(jonas[i], typeof jonas[i]);
+}
+
+console.log("--- BREAK WITH NUMBER ---");
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] === "number") {
+    break;
+  }
+
+  console.log(jonas[i], typeof jonas[i]);
+}
+
+// jonas.forEach((element) => {
+//   console.log(element);
+// });
